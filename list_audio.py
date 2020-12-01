04584f50bdb8d383c91b95241649f56514182f23
steffensen <steffensen@gmail.com>
2020-11-30 19:42:18 -0500

    pyaudio get_default_input_device_info()

--- list_audio.py
+++ list_audio.py
@@ -1,17 +1,18 @@
 import pyaudio
 
-def list_devices():
-    # List all audio input devices
-    p = pyaudio.PyAudio()
-    i = 0
-    n = p.get_device_count()
-    while i < n:
-        dev = p.get_device_info_by_index(i)
-        if dev['maxInputChannels'] > 0:
-            # print str(i)+'. '+dev['name']
-            print('Device {} is named {}'.format(i, dev['name']))
-        i += 1
+# List all audio input devices
+p = pyaudio.PyAudio()
+i = 0
+n = p.get_device_count()
+while i < n:
+    dev = p.get_device_info_by_index(i)
+    if dev['maxInputChannels'] > 0:
+        # print str(i)+'. '+dev['name']
+        print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print('Device {} is named {}'.format(i, dev['name']))
+    i += 1
 
 
-list_devices()
 
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print(p.get_default_input_device_info())

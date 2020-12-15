4b89e8199a761b57634e45cd1203a918de7319a9
steffensen <steffensen@gmail.com>
2020-12-14 22:08:28 -0500

    Added working bytes2ints, I think

--- detectZap.py
+++ detectZap.py
@@ -2,6 +2,15 @@ import numpy as np
 from scipy import signal
 import audioop
 
+
+#------------------------------------------------------------------------------
+# Convert PyAudio byte array into int16 array for scipy
+#------------------------------------------------------------------------------
+def bytes2ints(frame):
+    count = len(frame)/2
+    format = "%dh"%(count)
+    shorts = unpack( format, frame )
+
 #------------------------------------------------------------------------------
 # Calculates full FFT but only inspects high bins
 #------------------------------------------------------------------------------
@@ -11,11 +20,15 @@ def fullFft(v,threshold):
 	N  = len(v)
 	print('-------------------------------------------------------')
 	print('Length of input array = {}'.format(N))
+	print('Type: {}'.format(type(v)))
 	print('The array: {}'.format(v))
 	print('-------------------------------------------------------')
 	# int_values = [x for x in v]
-	int_values = int(v.encode('hex'), 16)
+	# int_values = int(v.encode('hex'), 16)
+	# int_values = v.hex
+	int_values = bytes2ints(v)
 	print('Length of int array = {}'.format(len(int_values)))
+	print('Type: {}'.format(type(int_values)))
 	print('The array: {}'.format(int_values))
 	print('-------------------------------------------------------')
 
